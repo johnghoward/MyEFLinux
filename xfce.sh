b6c3f0dc3745047e9c34c93d4600932d371a8114
John Howard <49537680+johnghoward@users.noreply.github.com>
2022-04-17 23:35:00 -0700

    Add files via upload

--- xfce.sh
+++ xfce.sh
@@ -3,7 +3,7 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-sudo reflector -c Switzerland -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector -c 'United States' -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 
 sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 sudo firewall-cmd --add-port=1025-65535/udp --permanent
@@ -20,10 +20,10 @@ makepkg -si --noconfirm
 #pikaur -S --noconfirm auto-cpufreq
 #sudo systemctl enable --now auto-cpufreq
 
-sudo pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies firefox simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc
+sudo pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies chromium simplescreenrecorder arc-gtk-theme arc-icon-theme mousepad xfburner gimp
 
-sudo flatpak install -y spotify
-sudo flatpak install -y kdenlive
+#sudo flatpak install -y spotify
+#sudo flatpak install -y kdenlive
 
 sudo systemctl enable lightdm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
